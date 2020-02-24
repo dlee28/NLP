7ad1268d5781aa91bcab88cda84ecae8e0b6e6b2
dlee28 <dongkeun@usc.edu>
2020-02-24 13:55:03 -0800

    reading training data correctly

--- 2/test.py
+++ 2/test.py
@@ -1,22 +1,129 @@
 import sys
-import os
-from collections import defaultdict
+import re
+file = sys.argv[1]
+import json
 
-if __name__ == '__main__':
+vaweightTrueOrFake = {}
+vaweightPosOrNeg = {}
+averageweightTrueOrFake = {}
+averageweightPosOrNeg = {}
+fTrueOrFake = {}
+fPosOrNeg={}
+c = 1
+array = []
+arrayb = []
+arrayc = []
 
-    print('Starting main ...')
+def deal(word):
+    for x in range(3,len(word)):
+        rule = re.compile(r'[^a-zA-z]')
+        word[x] = rule.sub("",word[x])
+        word[x] = word[x].lower()
 
-    input_path = sys.argv[1]
+def countnumfunc():
+    countword = {}
+    for x in range(3,len(word)):
+        if word[x] in countword:
+            countword[word[x]]+=1
+        else:
+            countword[word[x]]=1
+    return countword
 
-    positive_deceptive = []
-    positive_truthful = []
-    negative_deceptive = []
-    negative_truthful = []
 
-    training_data = defaultdict(list)
-    # print(type(training_data))
+def vanilla(weight,aveweight,isTrue,b,B,c):
+    total = 0
+    for x in countword:
+        if x in weight:
+            total += weight[x]*countword[x]
+            c = c + 1
+    total += b
+    mutitotal = isTrue*total
+    if mutitotal <= 0:
+        b += isTrue
+        B += isTrue*c
+        for x in countword:
+            if x in weight:
+                weight[x] =weight[x]+isTrue*countword[x]
+                aveweight[x] = aveweight[x]+isTrue*countword[x]*c
+            else:
+                weight[x]=isTrue*countword[x]
+                aveweight[x] = isTrue * countword[x] * c
 
-    for root, dirs, files in os.walk(input_path):
-        if len(dirs) == 0:
-            print(root, dirs, files)
+    #print(b)
+    return b,B,c
 
+
+# with open("train-labeled.txt","r") as f:
+with open(file, "r") as f:
+    content = f.readlines()
+    bvanillaTrueOrFake = 0
+    bvanillaNegOrPos = 0
+    BTrueOrFake = 0
+    BNegOrPos = 0
+    i = 0
+    while i < 30:
+        i += 1
+        for x in content:
+            x = x.strip()
+            word = x.split(" ")
+            array.append(word[0])
+            deal(word)
+            countword = countnumfunc()
+            if word[1] == 'Fake':
+                bvanillaTrueOrFake,BTrueOrFake,c = vanilla(vaweightTrueOrFake,averageweightTrueOrFake, -1, bvanillaTrueOrFake,BTrueOrFake,c)
+            else:
+                bvanillaTrueOrFake,BTrueOrFake,c = vanilla(vaweightTrueOrFake,averageweightTrueOrFake, 1, bvanillaTrueOrFake,BTrueOrFake,c)
+
+            if word[2] == 'Neg':
+                bvanillaNegOrPos,BNegOrPos,c = vanilla(vaweightPosOrNeg,averageweightPosOrNeg, -1, bvanillaNegOrPos,BNegOrPos,c)
+            else:
+                bvanillaNegOrPos,BNegOrPos,c = vanilla(vaweightPosOrNeg,averageweightPosOrNeg, 1, bvanillaNegOrPos,BNegOrPos,c)
+
+
+    for key in vaweightTrueOrFake:
+        fTrueOrFake[key] = vaweightTrueOrFake[key] - (1/float(c))*averageweightTrueOrFake[key]
+
+    for key in vaweightPosOrNeg:
+        fPosOrNeg[key] = vaweightPosOrNeg[key] - (1 / float(c)) * averageweightPosOrNeg[key]
+
+    averBTrueOrFake = bvanillaTrueOrFake - (1/float(c))*BTrueOrFake
+    averPosOrNeg = bvanillaNegOrPos - (1/float(c))*BNegOrPos
+
+    arrayb.append(bvanillaTrueOrFake)
+    print(bvanillaTrueOrFake)
+    arrayb.append(bvanillaNegOrPos)
+    print(bvanillaNegOrPos)
+    arrayc.append(averBTrueOrFake)
+    print(averBTrueOrFake)
+    arrayc.append(averPosOrNeg)
+    print(averPosOrNeg)
+
+
+
+
+import json
+listdic={}
+listdic["arrayb"]=arrayb
+listdic1={}
+listdic1["arrayb"]=arrayc
+jsObj1 = json.dumps(vaweightTrueOrFake)
+jsObj2= json.dumps(vaweightPosOrNeg)
+jsObj3 = json.dumps(listdic)
+jsObj4= json.dumps(fTrueOrFake)
+jsObj5= json.dumps(fPosOrNeg)
+jsObj6 = json.dumps(listdic1)
+
+fileObject = open("vanillamodel.txt","w")
+fileObject.write(jsObj1)
+fileObject.write('\n')
+fileObject.write(jsObj2)
+fileObject.write('\n')
+fileObject.write(jsObj3)
+
+
+fileObject = open("averagedmodel.txt","w")
+fileObject.write(jsObj4)
+fileObject.write('\n')
+fileObject.write(jsObj5)
+fileObject.write('\n')
+fileObject.write(jsObj6)
--- 3/perceplearn3.py
+++ 3/perceplearn3.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, re
 from _collections import defaultdict
 
 
@@ -9,22 +9,51 @@ if __name__ == '__main__':
 
     input_path = sys.argv[1]
 
-    training_data = defaultdict(list)
+    training_data = defaultdict(defaultdict)
+    all_words = []
+
     print('reading training data ...')
     for root, dirs, files in os.walk(input_path):
         if len(dirs) == 0:
             #root.endswith('4') or root.endswith('2') or root.endswith('3'):
             # or root.endswith('1') or root.endswith('2') or root.endswith('3')
             # print(root)
+
             for file in files:
-                if root.rfind('positive') >= 0 and root.rfind('deceptive') >= 0:
-                    training_data['positive_deceptive'].append(root + '/' + file)
-                elif root.rfind('positive') >= 0 and root.rfind('truthful') >= 0:
-                    training_data['positive_truthful'].append(root + '/' + file)
-                elif root.rfind('negative') >= 0 and root.rfind('deceptive') >= 0:
-                    training_data['negative_deceptive'].append(root + '/' + file)
-                elif root.rfind('negative') >= 0 and root.rfind('truthful') >= 0:
-                    training_data['negative_truthful'].append(root + '/' + file)
+
+                punctuation = '''[.,\/'"#!&\*;$?%\><^:{}=\-_`~()]'''
+                file_content = open(root + '/' + file).read()
+                file_content = re.sub(punctuation, '', file_content)  # get rid of punctuation in file
+                file_content = str.split(str.lower(file_content))
+
+                # for w in stop_words_dict:
+                #     while w in file_content:
+                #         file_content.remove(w)
+
+                training_data[root + '/' + file]
+
+                for word in file_content:
+                    if word not in training_data[root + '/' + file]:
+                        training_data[root + '/' + file][word] = 1
+                    else:
+                        training_data[root + '/' + file][word] += 1
+
+                    if word not in all_words:
+                        all_words.append(word)
     print('finished reading training data ...')
+
+    bias_pn  = 0
+    word_pn = {}
+    u_pn = {}
+    avg_pn = {}
+    beta_pn = 0
+
+    bias_td = 0
+    word_td = {}
+    u_td = {}
+    avg_td= {}
+    beta_td = 0
+    c = 1
+
 
     print('end main ...')
